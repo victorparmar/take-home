6abc96a9d86a145118ce67c27c263b5c16c38c85
Victor Parmar <vic@smalldata.tech>
2018-10-09 17:51:48 +0200

    Updates

---
+++ mysurance/precache-manifest.499d838473e822a3a2859a5252eba7e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2e88028e4ac1ff124d04647f7755253",
+    "url": "/take-home/mysurance/static/media/insurance-coverage.a2e88028.jpg"
+  },
+  {
+    "revision": "f730b6034bf8ad5f20eade4a88b25ae9",
+    "url": "/take-home/mysurance/static/media/funny-car-auto-insurance-quote.f730b603.png"
+  },
+  {
+    "revision": "24f6752532b3183b5e5e",
+    "url": "/take-home/mysurance/static/js/runtime~main.24f67525.js"
+  },
+  {
+    "revision": "fa739055e396e940efd1",
+    "url": "/take-home/mysurance/static/js/main.fa739055.chunk.js"
+  },
+  {
+    "revision": "3b523408d5934bb0a720",
+    "url": "/take-home/mysurance/static/js/1.3b523408.chunk.js"
+  },
+  {
+    "revision": "fa739055e396e940efd1",
+    "url": "/take-home/mysurance/static/css/main.e1bbf918.chunk.css"
+  },
+  {
+    "revision": "68a42619ca75ae76395ebca15072345e",
+    "url": "/take-home/mysurance/index.html"
+  }
+];
