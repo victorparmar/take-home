4159c294934750307ee4d8e191f5012bd30e2cda
Victor Parmar <vic@smalldata.tech>
2019-09-03 10:17:20 +0200

    Updates

---
+++ mysurance/precache-manifest.82b5bdfa9d55b182be640586fff94cf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc9ec1383c78bca7f0f55900f7a3775",
+    "url": "/take-home/mysurance/index.html"
+  },
+  {
+    "revision": "1ea4b3fe85dbb428f5c2",
+    "url": "/take-home/mysurance/static/css/main.bc4e7fdf.chunk.css"
+  },
+  {
+    "revision": "55a5ce270ce86834d68b",
+    "url": "/take-home/mysurance/static/js/2.57dd57e1.chunk.js"
+  },
+  {
+    "revision": "1ea4b3fe85dbb428f5c2",
+    "url": "/take-home/mysurance/static/js/main.c6d97623.chunk.js"
+  },
+  {
+    "revision": "324f1a20e01974cff4ee",
+    "url": "/take-home/mysurance/static/js/runtime~main.35892753.js"
+  },
+  {
+    "revision": "f730b6034bf8ad5f20eade4a88b25ae9",
+    "url": "/take-home/mysurance/static/media/funny-car-auto-insurance-quote.f730b603.png"
+  },
+  {
+    "revision": "a2e88028e4ac1ff124d04647f7755253",
+    "url": "/take-home/mysurance/static/media/insurance-coverage.a2e88028.jpg"
+  }
+]);
--- mysurance/service-worker.js
+++ mysurance/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/take-home/mysurance/precache-manifest.24284050670b479ecc5885fe673db2c2.js"
+  "/take-home/mysurance/precache-manifest.82b5bdfa9d55b182be640586fff94cf7.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/take-home/mysurance/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/take-home/mysurance/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
